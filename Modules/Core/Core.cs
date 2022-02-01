f39e0267eeacfa9c453697d4e259843a207f2e8b
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-02-01 10:02:02 -0500

    Check to see if PTG is enabled

--- Modules/Core/Core.cs
+++ Modules/Core/Core.cs
@@ -2,4 +2,5 @@ exec("./CreateBrick.cs");
 exec("./CustomBricks.cs");
 exec("./Extra.cs");
 exec("./Server_Ropes.cs");
-exec("./Support_FloatingBricks.cs");
+if (!$AddOn__System_PTG)
+    exec("./Support_FloatingBricks.cs");
