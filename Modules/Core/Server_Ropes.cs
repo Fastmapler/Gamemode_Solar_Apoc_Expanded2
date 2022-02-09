8421878e7a8cffc13e910877f3352ca43f5051e3
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-02-09 15:10:12 -0500

    Removes unused rtb config

--- Modules/Core/Server_Ropes.cs
+++ Modules/Core/Server_Ropes.cs
@@ -43,12 +43,7 @@ if(!isObject(MainRopeGroup))
 	new ScriptGroup(MainRopeGroup);
 }
 
-if(isFile("Add-Ons/System_ReturnToBlockland/server.cs") && !$RTB::Hooks::ServerControl)
-{
-	exec("Add-Ons/System_ReturnToBlockland/hooks/serverControl.cs");
-}
-
-RTB_registerPref("Slacked Rope Shapes", "Ropes", "Pref::Ropes::Iterations", "int 1 40", "Server_Ropes", 1, 0, 0);
+$Pref::Ropes::Iterations = 1;
 
 datablock StaticShapeData(RopeCylinder)
 {
