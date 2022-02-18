24aa419ac06fceaa9155f1070541bfcc153c0be8
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-02-18 16:30:31 -0500

    Plant Life (WIP)

--- Modules/Matter/Support_Plants.cs
+++ Modules/Matter/Support_Plants.cs
@@ -1,5 +1,7 @@
 function PlantLife_TickLoop()
 {
+    cancel($EOTW::PlantLifeLoop);
+
     if (!isObject(EOTWPlants) || EOTWPlants.getCount() < 1)
         return;
 
@@ -10,13 +12,16 @@ function PlantLife_TickLoop()
 
     for (%i = 0; %i < %totalTicks; %i++)
         EOTWPlants.getObject(getRandom(0, EOTWPlants.getCount() - 1)).EOTW_PlantLifeTick();
+
+    $EOTW::PlantLifeLoop = schedule(1000, EOTWPlants, "PlantLife_TickLoop");
 }
+$EOTW::PlantLifeLoop = schedule(1000, EOTWPlants, "PlantLife_TickLoop");
 
 function fxDtsBrick::EOTW_PlantLifeTick(%obj)
 {
     //Vines will attempt to grow along walls
     //Moss will attempt to grow along floors and ceilings. Will not stack vertically.
-    //Cacti (if implemented) will grow only grow upwards, up to a stack limit. Will not grow if horizontally adjacent to another cane block
+    //Cacti will grow only grow upwards, up to a stack limit. Will not grow if horizontally adjacent to another cane block
     //Both will only grow on blocks owned by the same bl_id
     //bl_id 888888 and 1337 are blacklisted completely
 
@@ -48,11 +53,16 @@ function fxDtsBrick::EOTW_PlantLifeTick(%obj)
     }
     else if (%data.getName() $= "brickEOTWCactiData")
     {
-        
+        %angleID = getRandom(0, 3);
+
+        %brick = CreateBrick(%client, %data, vectorAdd(%obj.getPosition(), "0 0 0.2"), %obj.getColorID(), %angleID);
+        if (isObject(%brick) && isObject(%brick.getDownBrick(15)) && %brick.getDownBrick(15).getDataBlock() == %data)
+            %brick.setDataBlock(brickEOTWDeadPlantData);
     }
     else if (%data.getName() $= "brickEOTWDeadPlantData")
     {
-        if (%obj.energy-- < -10)
+        %obj.energy--;
+        if (%obj.energy < -10)
             %obj.killBrick();
     }
 }
@@ -79,7 +89,7 @@ datablock fxDTSBrickData (brickEOTWMossData)
 
     isPlantBrick = true;
 };
-$EOTW::CustomBrickCost["brickEOTWMossData"] = 0.25 TAB "75ba6dff" TAB 4 TAB "Moss";
+$EOTW::CustomBrickCost["brickEOTWMossData"] = (1/4) TAB "75ba6dff" TAB 4 TAB "Moss";
 
 datablock fxDTSBrickData (brickEOTWCactiData)
 {
@@ -91,7 +101,7 @@ datablock fxDTSBrickData (brickEOTWCactiData)
 
     isPlantBrick = true;
 };
-$EOTW::CustomBrickCost["brickEOTWCactiData"] = 0.25 TAB "226027ff" TAB 8 TAB "Cacti";
+$EOTW::CustomBrickCost["brickEOTWCactiData"] = (1/8) TAB "226027ff" TAB 8 TAB "Cacti";
 
 datablock fxDTSBrickData (brickEOTWDeadPlantData)
 {
@@ -103,4 +113,4 @@ datablock fxDTSBrickData (brickEOTWDeadPlantData)
 
     isPlantBrick = true;
 };
-$EOTW::CustomBrickCost["brickEOTWCactiData"] = 0.25 TAB "75502eff" TAB 16 TAB "Wood";
+$EOTW::CustomBrickCost["brickEOTWCactiData"] = (1/16) TAB "75502eff" TAB 16 TAB "Wood";
