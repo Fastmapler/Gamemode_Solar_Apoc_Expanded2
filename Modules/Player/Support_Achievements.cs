b3a73797e33e9138676e58ad4ac8f93169532117
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-03-29 12:22:24 -0400

    Achievements (Early WIP)

---
+++ Modules/Player/Support_Achievements.cs
@@ -0,0 +1,37 @@
+forceRequiredAddOn("Server_Achievements");
+
+//Introductary
+AddServerAchievement("Materialistic","Gather any basic material from the ground.","NONE",0);
+AddServerAchievement("Industrial Engineering","Place any type of processing machine.","NONE",0);
+AddServerAchievement("Power to the Machines","Obtain a Cable Layer.","NONE",0);
+AddServerAchievement("Automation","Obtain a Pipe Layer.","NONE",0);
+AddServerAchievement("Go Craft a Real Weapon","Kill an enemy. Seriously, get an actual weapon so you don't torture yourself. A baseball bat even.","NONE",0);
+AddServerAchievement("An Actual Melee Weapon","Obtain any SWep melee weapon.","NONE",0);
+AddServerAchievement("A Modern Firearm","Obtain any Adventure Pack firearm.","NONE",0);
+
+//Industrial
+AddServerAchievement("Free Energy!","Place a solar panel.","NONE",0);
+AddServerAchievement("All Juiced Up","Inspect a Quad-Quad capactior you own that is capped off with energy.","NONE",0);
+//Fauna
+AddServerAchievement("Vermin Slayer","Kill 100 different enemies.","DisplayPoints",100);
+AddServerAchievement("Genocide","Kill 500 different enemies.","DisplayPoints",500);
+AddServerAchievement("A Key to Progression","Obtain the Boss Key.","NONE",0);
+AddServerAchievement("Zealous Victory","Defeat the Heirophant, master of all things tall and painful.","NONE",0);
+AddServerAchievement("Fiery Victory","Defeat the Lava Siren, who probably would of fared off better if she moved.","NONE",0);
+AddServerAchievement("Menacing Victory","Defeat the Sturdium Beast, won't buff Sturdium spawn rates unfortunately.","NONE",0);
+AddServerAchievement("Far Away From Home","Bring a Megafauna to the main island.","Hidden",0);
+
+//Materials
+AddServerAchievement("Magic Metal","Gather a Sturdium Stud.","NONE",0);
+AddServerAchievement("Blue Mist","Gather 400 units worth of Sturdium.","DisplayPoints",400);
+AddServerAchievement("Well Oiled","Gather some Crude Oil from an oil well.","NONE",0);
+AddServerAchievement("Stay Hydrated","Hold 1,000,000 units of water or more at once.","NONE",0);
+
+//Environment
+AddServerAchievement("That is indeed Lava","Try to go for a swim.","NONE",0);
+AddServerAchievement("Sky High","Go very high up.","NONE",0);
+AddServerAchievement("Rock Bottom","Touch the world's baseplate.","NONE",0);
+
+//Special
+AddServerAchievement("You shouldn't be here","Click the button ontop of the spawn.","HideDisc",0);
+AddServerAchievement("Not really that /gullible","Use the secret command.","HideTitle",0);
