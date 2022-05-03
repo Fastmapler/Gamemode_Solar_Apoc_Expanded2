9931d77d7611837091c5dc041fd79ff74848b449
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-05-03 13:14:32 -0400

    Dodgeball nerf/fix

--- Modules/Weapons/Weapons.cs
+++ Modules/Weapons/Weapons.cs
@@ -70,6 +70,36 @@ function updateWeaponDamage()
 }
 schedule(100, 0, "updateWeaponDamage");
 
+package EOTW_WeaponBalancing
+{
+    function dodgeballProjectile::onCollision(%this,%obj,%col,%fade,%pos,%normal)
+    {
+        //Play bounce sound and check that we haven't played it too recently
+        %obj.playSportBallSound( dodgeballBounceSound );
+        
+        // do onBallHit event, only call this on bricks obviously
+        if( %col.getType() & $TypeMasks::FxBrickObjectType )
+            %col.onBallHit( %obj.sourceObject, %obj );
+            
+        //Kill check
+        if(!%obj.hasBounced && %col.getType() & $TypeMasks::PlayerObjectType && minigameCanDamage(%obj.sourceObject, %col) == 1)
+        {
+            //do minigame checks then kill player use the cannon death image
+            //%col.kill();
+            %col.damage(%obj, %col.getposition(), 75, $DamageType::CannonBallDirect);
+        }
+        
+        // %speed = vectorLen( %obj.getVelocity() );
+
+        //Pass check
+        if( %obj.hasBounced &&  passBallCheck(%obj,%col) )
+            return;
+        
+        %obj.hasBounced = 1;
+        Projectile::onCollision(%this,%obj,%col,%fade,%pos,%normal);
+    }
+};
+activatePackage("EOTW_WeaponBalancing");
 
 
 //Theoretical weapon rework:
